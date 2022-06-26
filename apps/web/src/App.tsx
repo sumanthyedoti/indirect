bc191542814b327d956aa1477674029a9d701d9b
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-06-26 16:13:34 +0530

    change typescript jsx to react-jsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,19 @@
-import React, { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "@libs/components";
 
 import img from "./image.png";
 import logo from "./react-icon.svg";
 
 const App: FC = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <div>
       <h1>{process.env.NODE_ENV}</h1>
-      <Button label="LAUNCH APP" />
+      <h1>My App !</h1>
+      <Button
+        label={`clicks: ${count}`}
+        onClick={() => setCount((count) => count + 1)}
+      />
       <img src={img} width="500" alt="" />
       <img src={logo} width="500" alt="" />
     </div>
--- apps/web/webpack/webpack.dev.js
+++ apps/web/webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
@@ -17,7 +18,12 @@ module.exports = merge(
           exclude: /node_modules/,
           use: [
             {
-              loader: "ts-loader",
+              loader: "babel-loader",
+              options: {
+                plugins: [require.resolve("react-refresh/babel")].filter(
+                  Boolean
+                ),
+              },
             },
           ],
         },
@@ -41,8 +47,9 @@ module.exports = merge(
       open: true,
     },
     plugins: [
+      new ReactRefreshWebpackPlugin(),
       // new BundleAnalyzerPlugin(),
-    ],
+    ].filter(Boolean),
   },
   commonConfig
 );
