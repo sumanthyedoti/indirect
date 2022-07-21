e55f467d1abf5962bcfd7f445e21b5d0fdba256b
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-07-21 14:54:44 +0530

    @apps/server: Modify profile table for status

--- apps/server/src/types/spaces.ts
+++ apps/server/src/types/spaces.ts
@@ -12,6 +12,21 @@ export interface Space extends Info {
 
 export interface UpdateSpace extends Info {}
 
+export interface SpaceUser {
+  id: number
+  user_id: number
+  space_id: number
+  fullname: string
+  display_name: string | null
+  email: string
+  status_text: string | null
+  status_emoji: string | null
+  status_duration: string | null
+  is_active: boolean
+  created_at: Date
+  updated_at: Date
+}
+
 export enum Constraints {
   name = 60,
   tagline = 100,
