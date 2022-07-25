4dccca729d28a96b01fd846a9fc9b2f2dc3316c0
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-07-25 14:52:40 +0530

    Refactor components structure

--- apps/web/src/components/Main/MainHeader.tsx
+++ apps/web/src/components/Main/MainHeader.tsx
@@ -4,8 +4,8 @@ import userStore from '../../store/userStore'
 import { useQueryChannel } from '../../queries'
 
 const SideHeader: FC = () => {
-  const { spaceId } = userStore()
-  const { data: channel, isSuccess } = useQueryChannel(spaceId)
+  const { channelId } = userStore()
+  const { data: channel, isSuccess } = useQueryChannel(channelId)
   if (!isSuccess) return null
   return (
     <h2 className="mb-0 text-base border-b border-gray-600 side-panel-padding">
--- apps/web/src/components/organisms/index.ts
+++ apps/web/src/components/organisms/index.ts
@@ -1,3 +1,2 @@
 export { default as MessagesOfADay } from './MessagesOfADay'
 export { default as Modal } from './Modal'
-export { default as CreateChannel } from './CreateChannel'
