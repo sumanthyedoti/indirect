903a9d3c777ba61c47e0e43c71a6b72bcd1c44ff
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-08-02 10:46:29 +0530

    @apps/web: Create Space from Spaces view

--- apps/web/src/views/Spaces.tsx
+++ apps/web/src/views/Spaces.tsx
@@ -1,24 +1,62 @@
-import { FC } from 'react'
+import { useState, useCallback, FC } from 'react'
+import { useQueryClient } from 'react-query'
+import toast from 'react-hot-toast'
 
+import Modal from '../components/Modal'
+import { Space as SpaceT, CreateSpace as CreateSpaceT } from '@api-types/spaces'
 import { Button } from '../components/atoms'
 import { useQueryUserSpaces } from '../queries'
+import CreateSpace from '../components/SpacesBar/CreateSpace'
 import { useUserStore } from '../store'
+import api from '../axios'
 
 interface SpacesProps {
   dummy?: null
 }
 
 const Spaces: FC<SpacesProps> = () => {
+  const queryClient = useQueryClient()
+  const { setSpaceId } = useUserStore()
+  const [isCreateSpaceModalOpen, setIsCreateSpaceModalOpen] = useState(false)
   const { user } = useUserStore()
   const { data: spaces } = useQueryUserSpaces(user?.id)
+  const onCreateNewSpace = useCallback(async (data: CreateSpaceT) => {
+    try {
+      const {
+        data: { data: newSpace },
+      } = await api.post<{ data: SpaceT }>('/spaces', data)
+      toast.success('Space created', {
+        id: 'post-space-success',
+      })
+      queryClient.setQueryData<SpaceT[] | undefined>(
+        'spaces',
+        //@ts-ignore
+        (spaces) => {
+          if (!spaces) return newSpace
+          return [...spaces, newSpace]
+        }
+      )
+      queryClient.invalidateQueries('spaces')
+      setIsCreateSpaceModalOpen(false)
+      setSpaceId(newSpace.id)
+    } catch (err) {
+      console.log(err)
+      toast.error('Error creating Space', {
+        id: 'post-spacel-error',
+      })
+    }
+  }, [])
   if (!spaces) return null
   return (
     <div className="p-5 mx-auto sm:px-5 md:px-0 md:w-4/5 lg:w-3/5">
-      <div className="flex flex-col justify-between mb-10 sm:flex-row sm:items-center">
+      <div className="flex flex-col justify-between mb-14 sm:flex-row sm:items-center">
         <h1 role="banner" className="text-4xl font-light text-white sm:mb-0">
           inDirect
         </h1>
-        <Button label="Create a new Space" />
+        <Button
+          onClick={() => setIsCreateSpaceModalOpen(true)}
+          label="Create a new Space"
+        />
       </div>
       <h1>Your Spaces</h1>
       <article className="rounded bg-slate-800">
@@ -37,6 +75,15 @@ const Spaces: FC<SpacesProps> = () => {
           )
         })}
       </article>
+      <Modal
+        isOpen={isCreateSpaceModalOpen}
+        close={() => setIsCreateSpaceModalOpen(false)}
+      >
+        <CreateSpace
+          createSpace={onCreateNewSpace}
+          close={() => setIsCreateSpaceModalOpen(false)}
+        />
+      </Modal>
     </div>
   )
 }
