22d8c90fef3fae30ac809d1af994ada41841b7b2
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-08-10 16:17:33 +0530

    web: Refactor react-window row with MessageRow

--- apps/web/src/components/Channel/ChannelMessages.tsx
+++ apps/web/src/components/Channel/ChannelMessages.tsx
@@ -1,8 +1,8 @@
-import { useRef, FC, useEffect } from 'react'
+import { useRef, FC, useEffect, memo } from 'react'
 import { VariableSizeList as List } from 'react-window'
 import AutoSizer from 'react-virtualized-auto-sizer'
 
-// import * as T from '@api-types/messages'
+import * as T from '@api-types/messages'
 // import MessagesOfADay from './MessagesOfADay'
 import MessageDate from '../atoms/MessageDate'
 import Message from '../molecules/Message'
@@ -10,11 +10,59 @@ import useUserStore from '../../store/useUserStore'
 // import useSocket from '../../hooks/useSocket'
 import { useQueryChannelMessages, useQuerySpaceUsers } from '../../queries'
 
+interface MessageRowProps {
+  index: number
+  messages: T.Message[]
+  style: any
+  setRowHeight: (index: number, size: number) => void
+}
+const MessageRow: FC<MessageRowProps> = memo(
+  ({ index: i, messages, style, setRowHeight }: any) => {
+    const rowRef = useRef<HTMLDivElement>(null)
+    const { spaceId } = useUserStore()
+    const { data: users } = useQuerySpaceUsers(spaceId)
+    useEffect(() => {
+      if (rowRef.current) {
+        // console.log(
+        //   rowRef.current.clientHeight,
+        //   rowRef.current.dataset.i,
+        //   rowRef.current.getBoundingClientRect().height
+        // )
+        setRowHeight(i, rowRef.current.getBoundingClientRect().height)
+      }
+      // eslint-disable-next-line
+    })
+    const prevDate = messages[i - 1]
+      ? new Date(messages[i - 1].created_at).getDate()
+      : null
+    const currentDate = new Date(messages[i]?.created_at).getDate()
+    if (!users) return null
+    return (
+      <div ref={rowRef} data-i={i} style={style}>
+        {currentDate !== prevDate && (
+          <div className="pb-0 mt-3 border-t border-gray-500">
+            <MessageDate timestamp={messages[i].created_at} />
+          </div>
+        )}
+        <Message
+          key={messages[i].id}
+          createdAt={messages[i].created_at}
+          senderName={
+            users.idMap[messages[i].sender_id]?.display_name ||
+            users.idMap[messages[i].sender_id]?.fullname
+          }
+          message={messages[i]}
+        />
+      </div>
+    )
+  }
+)
+MessageRow.displayName = 'MessageRow'
+
 const ChannelMessages: FC = () => {
-  const { channelId, spaceId } = useUserStore()
+  const { channelId } = useUserStore()
   const listRef = useRef(null)
   const rowHeights = useRef<Record<number, number>>({})
-  const { data: users } = useQuerySpaceUsers(spaceId)
   const { data: messages } = useQueryChannelMessages(channelId)
 
   // const socket = useSocket()
@@ -51,40 +99,6 @@ const ChannelMessages: FC = () => {
 
   if (!messages) return null
 
-  const Row = ({ index: i, style }: any) => {
-    const rowRef = useRef<HTMLDivElement>(null)
-    useEffect(() => {
-      if (rowRef.current) {
-        console.log(rowRef.current.clientHeight, rowRef.current.dataset.i)
-        setRowHeight(i, rowRef.current.clientHeight)
-      }
-      // eslint-disable-next-line
-    }, [rowRef])
-    const prevDate = messages[i - 1]
-      ? new Date(messages[i - 1].created_at).getDate()
-      : null
-    const currentDate = new Date(messages[i]?.created_at).getDate()
-    if (!users) return null
-    return (
-      <div ref={rowRef} data-i={i} style={style}>
-        {currentDate !== prevDate && (
-          <div className="pb-0 mt-3 border-t border-gray-500">
-            <MessageDate timestamp={messages[i].created_at} />
-          </div>
-        )}
-        <Message
-          key={messages[i].id}
-          createdAt={messages[i].created_at}
-          senderName={
-            users.idMap[messages[i].sender_id]?.display_name ||
-            users.idMap[messages[i].sender_id]?.fullname
-          }
-          message={messages[i]}
-        />
-      </div>
-    )
-  }
-
   return (
     <article
       className={`h-full overflow-y-auto
@@ -101,7 +115,14 @@ const ChannelMessages: FC = () => {
               itemCount={messages.length}
               itemSize={getRowHeight}
             >
-              {Row}
+              {({ index, style }) => (
+                <MessageRow
+                  messages={messages}
+                  index={index}
+                  style={style}
+                  setRowHeight={setRowHeight}
+                />
+              )}
             </List>
           )
         }}
