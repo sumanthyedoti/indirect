74cb79c53d2793af1257b46ef8f58d2998a7424a
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-08-15 19:02:36 +0530

    web: add api base URL based on envrironment

--- apps/web/src/axios.ts
+++ apps/web/src/axios.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+const env = process.env.NODE_ENV || 'development'
+
 export default axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL:
+    env === 'development'
+      ? 'http://localhost:8000'
+      : 'https://indirect-app.herokuapp.com',
   withCredentials: true,
 })
